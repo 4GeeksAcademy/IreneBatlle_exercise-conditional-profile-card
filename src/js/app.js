2c36a4ca845c82d07cdd2100272723f707fa35d6
Irene Batlle Mola <ibatllemola@gmail.com>
2024-12-04 11:29:49 +0000

    arreglos de 4 fallos

--- src/js/app.js
+++ src/js/app.js
@@ -39,18 +39,14 @@ function render(variables = {}) {
           <h2>${variables.role || "Posición"}</h2>
           <h3>${variables.city || "Ciudad"}, ${variables.country || "País"}</h3>
           <ul class=${variables.socialMediaPosition}>
-            <li><a href="https://twitter.com/${
-              variables.twitter
-            }"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/${
-              variables.github
-            }"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/${
-              variables.linkedin
-            }"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/${
-              variables.instagram
-            }"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/${variables.twitter ||
+              "4geeksacademyes"}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github ||
+              "4geeksacademy"}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${variables.linkedin ||
+              "school/4geeksacademyes"}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram ||
+              "4geeksacademy"}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
